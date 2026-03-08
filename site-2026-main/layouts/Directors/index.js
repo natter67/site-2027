c3d802117362f142d37104e7ee19d57b813b82e4
natter67 <nsmileschan@gmail.com>
2026-03-08 13:21:14 -0500

    Director's Note and Committee Photo

--- site-2026-main/layouts/Directors/index.js
+++ site-2026-main/layouts/Directors/index.js
@@ -13,25 +13,25 @@ export default function Directors() {
             {/* Directors' note spans the width of the image */}
             <div className="w-full max-w-3xl border p-6 rounded-lg">
                 <p className="mt-4">
-                    Welcome to the 103rd Engineering Open House (EOH) at the University of Illinois Urbana-Champaign! We are so excited to welcome university students, faculty, community members, and field trips to campus. EOH is the nation’s largest student-run STEM (Science, Technology, Engineering, and Math) fair, where participants have the opportunity to showcase independent engineering projects and innovations unseen by the scientific community.                 </p>
+                    Welcome to the 104th Engineering Open House (EOH) at the University of Illinois Urbana–Champaign! Whether this is your first visit or your tenth, we are incredibly excited to welcome you all to the nation’s largest student-run STEM (Science, Technology, Engineering, and Math) fair and to showcase how the Grainger College of Engineering is <b>Forging the Future</b>.                 </p>
                 <p className="mt-2">
-                    The theme of this year’s event is The Age of Innovation. It is our hope that EOH visitors will foster new curiosity in STEM and learn more about how students at the Grainger College of Engineering are at the forefront of innovation and scientific excellence. 
+                    For EOH 2026, we chose the theme <b>Forging the Future</b> to highlight the central role that Grainger’s engineers hold in creating and shaping the technologies that will define tomorrow. With 210 exhibits, 19 special events, and 15 engineering startups, most of which were designed and built entirely by current students, we proudly display the creativity, innovation, and ambition of engineering students and faculty at UIUC. Through these efforts, we not only demonstrate the limitless possibilities within STEM, but also aim to inspire the next generation of engineers to imagine and build the future themselves.
                 </p>
                 <p className="mt-5">
-                    EOH 2025 features over 200 exhibits, including 10 student startups, and 14 special events such as rocket launches, student-engineered cars, and escape rooms. Each exhibit at EOH has been many months in the making, and the majority of projects are entirely student-led. EOH features work from every engineering discipline, celebrating not only current technologies but ushering in a new Age of Innovation.
+                    Behind every exhibit, event, competition, and showcase at EOH stands a community of individuals whose efforts begin long before the doors open. From exhibitors and volunteers to corporate sponsors, faculty, staff, judges, alumni, and Grainger administration, this event is driven by hard work and a shared belief in the value of hands-on innovation and student leadership. The constant support of all of those involved strengthens a tradition that has inspired engineers for over a century.
                 </p>
                 <p className="mt-2">
-                    This year, EOH has the special opportunity to be a part of the festivities of Mom’s Weekend. We would like to give all Grainger Engineering Moms a warm welcome, and invite you to explore the unique features of EOH that have been organized in celebration. 
+                    At the heart of this weekend is the EOH Central Committee, composed of 29 student leaders who dedicate nearly a full year to transforming ideas into reality. Balancing coursework, research, internships, and personal commitments while leading an entirely student-run event of this scale is no small undertaking, and we thank our incredible committee for their true dedication and perseverance. Their organization, teamwork, and creativity are what make Engineering Open House not just possible, but exceptional.
                 </p>
                 <p className="mt-2">
-                    EOH would not be possible without exhibitors, volunteers, corporate sponsors, judges, alumni, faculty, staff, and Grainger administration. We thank you for your dedication, hard work, creativity, and resilience in the face of challenges. Our largest thank you goes to the EOH Central Committee, composed of 29 directors all responsible for a unique aspect of EOH. EOH is an entirely student-led event, and planning has been in full force for almost an entire calendar year. We rely on the Central Committee to plan EOH on top of being full-time students — an immense responsibility that we would like to express our deepest gratitude for. 
+                    This year, the EOH committee has worked tirelessly to implement novel ideas towards <b>Forging the Future</b> of EOH. 2026 marks the debut of the EOH mobile app, opening new pathways for visitors to engage with and experience our event. In addition, our Startup Showcase competition has grown larger than ever, promoting entrepreneurship in engineering through new collaborations with some of the most impactful Illinois startup success stories. With the introduction of EOH’s Research Showcase, we recognize the significant contributions to cutting-edge science and technology made by undergraduate researchers at Illinois. Finally, a new commitment to sustainability in materials and practices ensures that we forge thoughtfully, creating an event that will inspire scientists and engineers for generations to come.
                 </p>
                 <p className="mt-2">
-                    We thank every single person for being a part of EOH and celebrating what it means to be a Grainger Engineer. We hope that you explore engineering and appreciate the new Age of Innovation that is happening right here at the University of Illinois.
+                    We greatly appreciate each and every one of our visitors for their support year after year, from students, faculty, and community members here in Champaign–Urbana to those traveling from across the state and beyond. We’d also like to extend an additional warm welcome to those joining us for Mom’s Weekend and invite all guests to discover the unique events and experiences of EOH. As you explore, we encourage you to ask questions, stay curious, and immerse yourself in the innovation around you. We look forward to <b>Forging the Future</b> together.
                 </p>
                 <p className="mt-2">
-                    Alicia Kim & Alyssa Huang <br />
-                    Co-Directors, Engineering Open House 2025
+                    Aparna Kamath & Maddie Conrad <br />
+                    Co-Directors, Engineering Open House 2026
                 </p>
             </div>
         </div>
