aebf36715b8dff56c1ece7b07e61b2d21c84547d
Vani Ramesh <53530682+share-a-byte@users.noreply.github.com>
2026-03-23 18:45:24 -0500

    updating slack join link

--- site-2026-main/pages/volunteer/index.js
+++ site-2026-main/pages/volunteer/index.js
@@ -195,7 +195,7 @@ export default function VolunteerPortalReserved() {
         </div> 
         <div> 
           <p className="font-semibold">💬 Join the Slack</p> 
-          <p className="text-sm mt-1"> Stay updated and ask questions in our Slack group. <br /> <a href="https://join.slack.com/t/eohvolunteering2026/shared_invite/zt-3q5ubhpeo-McpC8WsbTppCBf9Rhb0UOQ" target="_blank" rel="noopener noreferrer" className="text-blue-600 underline" > Join Slack → </a> </p> 
+          <p className="text-sm mt-1"> Stay updated and ask questions in our Slack group. <br /> <a href="https://join.slack.com/t/eohvolunteering2026/shared_invite/zt-3toepywds-prURsU8Buq1uOJ9Z285q1A" target="_blank" rel="noopener noreferrer" className="text-blue-600 underline" > Join Slack → </a> </p> 
         </div> 
       </div>
 
